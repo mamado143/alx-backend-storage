39f682dfbb4580133864d2f6bc794de9625bf67a
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-07-13 22:55:39 +0300

    solve:  problem  4

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+-- Lists all bands with Glam rock.
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
